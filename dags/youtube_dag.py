53a980b7671e97021bf85905bf88bb5f4523a718
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-04-05 23:09:15 -0400

    final commit

--- dags/youtube_dag.py
+++ dags/youtube_dag.py
@@ -8,6 +8,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import date
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -15,16 +16,13 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
-
 def insert_raw_data():
     engine = sqlalchemy.create_engine('mysql+pymysql://root:zipcoder@localhost/testDB')
     df = pd.read_csv("/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/youtube-new/USvideos.csv",
                      delimiter=',')
 
-
     with engine.connect() as conn, conn.begin():
-        df.to_sql('info2', conn, if_exists='replace')
-
+        df.to_sql('raw_data', conn, if_exists='replace')
 
 
 def category_data():
@@ -46,23 +44,27 @@ def category_data():
 
 def agg_data():
     df = pd.read_csv('/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/youtube-new/USvideos.csv')
-    months = {1:"January",2:"February",3:"March",4:"April",5:"May",6:"June",7:"July",8:"August",
-              9:"September",10:"October",11:"November",12:"December"}
+    months = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August",
+              9: "September", 10: "October", 11: "November", 12: "December"}
     df['month'] = df.apply(lambda row: months[int(row.trending_date.split(".")[2])], axis=1)
     df11 = pd.read_csv(
         '/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/dataclean/US_category_formatted.csv')
-    #df.groupby('category_id').count()
+    # df.groupby('category_id').count()
     df12 = df11.astype({'category_id': int})
     merged_inner = pd.merge(df, df12, on='category_id')
     df13 = merged_inner.groupby(['month', 'category_title']).count()
-    with open('/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/dataclean/USvideos_aggregated.csv',
-              'w') as f:
+    with open(
+            '/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/dataclean/USvideos_aggregated.csv',
+            'w') as f:
         f.write(df13.to_csv())
 
 
 def save_report():
-    df13 = pd.read_csv('/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/dataclean/USvideos_aggregated.csv')
-    df14 = df13.iloc[:, 0:1]
+    df13 = pd.read_csv(
+        '/Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow/dataclean/USvideos_aggregated.csv',
+        encoding='utf-8-sig')
+    df14 = df13.iloc[:, 0:3]
+    df14.columns.str.strip()
     fig, ax = plt.subplots(figsize=(15, 7))
     df14.groupby(['month', 'category_title']).sum()['video_id'].unstack().plot(ax=ax)
     ax.set_xlabel('Month')
@@ -90,8 +92,10 @@ def save_report():
 #     s.sendmail(fromaddr, toaddr, text)
 #     s.quit()
 
+
 def send_report():
     return "send report"
+
 
 default_args = {
     'owner': 'Apoorva',
@@ -109,8 +113,6 @@ dag = DAG(
     description='Youtube Trending Videos Analysis',
 )
 
-
-
 t1 = BashOperator(
     task_id='download_from_source',
     bash_command='kaggle datasets download datasnaek/youtube-new -p /Users/amishra/DEV/DataEngineering.Labs.AirflowProject/DataEngg-Airflow',
@@ -124,24 +126,19 @@ t2 = BashOperator(
 )
 
 t3 = PythonOperator(
-    task_id='Persist_data',
+    task_id='Persist_data_In_DB',
     python_callable=insert_raw_data,
     dag=dag,
 )
-# t3 = PythonOperator(
-#     task_id='ReadFile',
-#     python_callable=read_data,
-#     dag=dag,
-# )
 
 t4 = PythonOperator(
-    task_id='CombiningData',
+    task_id='CombiningDataForAnalysis',
     python_callable=category_data,
     dag=dag,
 )
 
 t5 = PythonOperator(
-    task_id='AggregatingData',
+    task_id='Aggregate_data',
     python_callable=agg_data,
     dag=dag,
 )
@@ -153,15 +150,26 @@ t6 = PythonOperator(
 )
 
 t7 = PythonOperator(
-    task_id='SendReport',
-     python_callable=send_report,
+    task_id='SendReportViaEmail',
+    python_callable=send_report,
     dag=dag,
 
 )
 
+#
+# t4 = PythonOperator(
+#     task_id='merge_data',
+#     python_callable=category_data,
+#     dag=dag,
+# )
 
 
-
+# t7 = PythonOperator(
+#     task_id='SendReport',
+#     python_callable=send_report,
+#     dag=dag,
+#
+# )
 
 
 t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7
